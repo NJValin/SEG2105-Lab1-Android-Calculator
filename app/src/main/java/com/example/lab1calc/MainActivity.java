3d3a7f9f756f65161631181257f36b8e670a3329
Neil Valin <nvali069@uottawa.ca>
2022-09-29 17:05:07 -0400

    Initialize Repository

---
+++ app/src/main/java/com/example/lab1calc/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab1calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnl;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //btn1 = findViewById(R.id.btn1);
+    }
+}
