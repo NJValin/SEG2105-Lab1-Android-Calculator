c06097dfda62341b9b7588747889ddd05438a655
mahamedfazeli <mfazeli125@gmail.com>
2022-11-06 16:03:27 -0500

    working test

--- app/src/test/java/com/example/lab1calc/EvaluateTest.java
+++ app/src/test/java/com/example/lab1calc/EvaluateTest.java
@@ -1,7 +1,27 @@
 package com.example.lab1calc;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-public class EvaluateTest extends TestCase {
+import org.junit.Test;
+
+import javax.script.ScriptException;
+
+public class EvaluateTest {
+
+    @Test
+    public void evaluate_add() throws ScriptException {
+        Evaluate e = new Evaluate();
+        double value = e.evaluate("8+8").doubleValue();
+        double expectedValue = 16;
+        assertEquals( "test addition failed",expectedValue,value, 1e-3);
+    }
+
+    @Test
+    public void evaluate_Division() throws ScriptException {
+        Evaluate e = new Evaluate();
+        double value = e.evaluate("8+8").doubleValue();
+        double expectedValue = 16;
+        assertEquals( "test addition failed",expectedValue,value, 1e-3);
+    }
 
 }
